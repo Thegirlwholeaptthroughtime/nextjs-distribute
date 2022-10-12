378bf8708de761e8a17433e5af66b4d450e2cbcd
DH Kang <94300119+Thegirlwholeaptthroughtime@users.noreply.github.com>
2022-10-12 23:52:36 +0900

    1

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Next js 배포 연습입니다
+          Next js 배포 연습입니다1221
         </h1>
 
         <p className={styles.description}>
